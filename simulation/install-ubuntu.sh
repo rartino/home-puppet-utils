67b90a9941d0cad5c81730d2e1ec72eefb4f890a
Rickard Armiento <rickard-gpg@armiento.net>
2021-03-20 11:27:12 +0100

    Require system_id for provision

--- simulation/install-ubuntu.sh
+++ simulation/install-ubuntu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 <system_id>"
+    exit 1
+fi
+
 wget https://raw.githubusercontent.com/rartino/home-puppet-control/production/bin/provision-ubuntu.sh
 chmod +x provision-ubuntu.sh
 sudo ./provision-ubuntu.sh
