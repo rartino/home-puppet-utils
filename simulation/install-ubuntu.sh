eb20666e2a0bd0b6cb31c12d932464f1eb5b4b69
Rickard Armiento <rickard-gpg@armiento.net>
2021-03-20 11:35:14 +0100

    Improved install script

--- simulation/install-ubuntu.sh
+++ simulation/install-ubuntu.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <system_id>"
+if [ -z "$1" -o -z "$2" ]; then
+    echo "Usage: $0 <provision_script_url> <system_id>"
+    echo
+    echo "E.g.:"
+    echo "  $0 https://gitlab.liu.se/mdi/puppet-control/raw/production/bin/provision-ubuntu.sh hse"
+    echo "  $0 https://raw.githubusercontent.com/rartino/home-puppet-control/production/bin/provision-ubuntu.sh minimoose"
+    echo
     exit 1
 fi
 
-wget https://raw.githubusercontent.com/rartino/home-puppet-control/production/bin/provision-ubuntu.sh
+wget "$1" -o provision-ubuntu.sh
 chmod +x provision-ubuntu.sh
-sudo ./provision-ubuntu.sh
+sudo ./provision-ubuntu.sh "$2"
